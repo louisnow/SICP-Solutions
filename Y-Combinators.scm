cfabfbafe2017a51752e045f8aed03f8044f4fa4
Louis Christopher <louis9171@outlook.com>
2016-04-16 12:53:11 +0530

    Fix formatting and add more context

--- Y-Combinators.scm
+++ Y-Combinators.scm
@@ -3,14 +3,10 @@
 ;this magical program which eventually leads to Y Combinators blows my mind
 ;For a deeper understanding on how this works visit, http://mvanier.livejournal.com/2897
 ;GCD using euclid's algorithm
+;I could have renamed myself to gcd but I think it's easier to reason about this way for now
 (((lambda (f)( f f))
  (lambda (myself)
   (lambda (a b)
     (cond
       ((= b 0) a)
-      (else((myself myself) b (modulo a b)))
-      )
-    )
-  )
- )
-10 5)
+      (else((myself myself) b (modulo a b)))))))10 5)
