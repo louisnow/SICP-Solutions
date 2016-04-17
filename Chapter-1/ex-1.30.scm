5b1621ff30261cbc986e43b3447888fb3ce718da
Louis Christopher <louis9171@outlook.com>
2016-04-17 14:40:33 +0530

    Add ex-1.30
    Also added another type of swap file fedora seems to create to gitignore

---
+++ Chapter-1/ex-1.30.scm
@@ -0,0 +1,10 @@
+#lang racket
+;ex 1.30
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+;iterative sum function,1+2+3 = 6
+(sum (lambda (x) x) 1 (lambda (x)(+ x 1)) 3)
